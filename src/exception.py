fe49a582141a510a858caf5221ac1522d8338369
Zehra <zehraarshadmulla@gmail.com>
2024-08-07 20:42:39 +0500

    Data Ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
 import sys
-import logging
+# from src.logger import logging
+
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
@@ -10,7 +11,8 @@ class CustomException(Exception):
     def error_message_detail(self, error_message, error_detail:sys):
         super().__init__(error_message)
         self.error_message=error_message_detail(error_message, error_detail=error_detail)
+
     def __str__(self):
         return self.error_message
-    
+
 
